8e5b135b9abc5fe2d53c00e9fe81e809e242fd42
mutanabbi <ravishankar@mail.ru>
2021-09-22 01:17:16 +0300

    A modulator's modulator

--- src/waves/main.cpp
+++ src/waves/main.cpp
@@ -6,11 +6,23 @@ int main()
     SawToothWave saw_gen {WaveArgs::Builder {}.frequency(1.), tp_gen};
     SawToothWave inverse_saw_gen {WaveArgs::Builder {}.frequency(1.), tp_gen};
 
-    SineWave mod_sine {WaveArgs::Builder {}.frequency(1).y_offset(10).amp(20), tp_gen};
-    SineWave sine_gen {WaveArgs::Builder {}.frequency(mod_sine), tp_gen};
+    SineWave mod_mod_sine {WaveArgs::Builder{}
+        .frequency(1)
+        .y_offset(1)
+        .amp(2)
+      , tp_gen
+    };
+    SineWave mod_sine {WaveArgs::Builder{}
+        .frequency(mod_mod_sine)
+        .amp(mod_mod_sine)
+        .y_offset(1)
+        .amp(2)
+      , tp_gen
+    };
+    SineWave sine_gen {WaveArgs::Builder{}.frequency(mod_sine), tp_gen};
 
-    OneLineRenderer term_renderer {80, -1., 1.};
-    Sampler sampler {sine_gen, term_renderer, 180};
+    OneLineRenderer term_renderer {100, -1., 1.};
+    Sampler sampler {sine_gen, term_renderer, 150};
     sampler.start();
     sampler.run();
 }
