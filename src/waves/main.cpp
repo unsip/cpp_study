ca8a3a3be1c6d5b5e392a87300763a597fb8cde3
unsip <unsip@tutamail.com>
2021-09-21 23:56:35 +0300

    Frequency modulation via Builder.

--- src/waves/main.cpp
+++ src/waves/main.cpp
@@ -3,13 +3,14 @@
 int main()
 {
     TimePointGen tp_gen;
-    SawToothWave saw_gen {WaveArgs::Builder {}.frequency(1.).phase(3.14 / 2), tp_gen};
-    SawToothWave inverse_saw_gen {WaveArgs::Builder {}.frequency(1.).amp(-1.), tp_gen};
-    SineWave sine_gen {WaveArgs::Builder {}.frequency(0.5), tp_gen};
-    //SquareWave square_gen {sine_gen};
-    RingModulator ring {saw_gen, inverse_saw_gen};
-    OneLineRenderer term_renderer {50, -3., 3.};
-    Sampler sampler {inverse_saw_gen, term_renderer, 50};
+    SawToothWave saw_gen {WaveArgs::Builder {}.frequency(1.), tp_gen};
+    SawToothWave inverse_saw_gen {WaveArgs::Builder {}.frequency(1.), tp_gen};
+
+    SineWave mod_sine {WaveArgs::Builder {}.frequency(.1).y_offset(.5).amp(.5), tp_gen};
+    SineWave sine_gen {WaveArgs::Builder {}.frequency(mod_sine), tp_gen};
+
+    OneLineRenderer term_renderer {80, -1., 1.};
+    Sampler sampler {sine_gen, term_renderer, 20};
     sampler.start();
     sampler.run();
 }
--- src/waves/wave_gen.hpp
+++ src/waves/wave_gen.hpp
@@ -18,26 +18,39 @@ public:
 class WaveArgs
 {
 private:
-    double m_step = 0.;
-    double m_amp = 1.;
-    // Hertz
-    double m_frequency = 1.;
-    double m_phase = 0.;
-    double m_y_offset = 0.;
     WaveArgs() = default;
+
+    class Arg
+    {
+        const Generator<double>* m_gen = nullptr;
+        double m_value = 0.;
+    public:
+        Arg(double v) : m_value(v) {}
+        Arg(const Generator<double>& gen) : m_gen(&gen) {}
+
+        double value() const
+        {
+            return m_gen != nullptr ? m_gen->get() : m_value;
+        }
+    };
+
+    Arg m_step = Arg {0.};
+    Arg m_amp = Arg {1.};
+    // Hertz
+    Arg m_frequency = Arg {1.};
+    Arg m_phase = Arg {0.};
+    Arg m_y_offset = Arg {0.};
 public:
     class Builder;
 
-    // Discrete step over wave
-    double step() const { return m_step; }
     // Wave amplitude size
-    double amp() const { return m_amp; }
+    double amp() const { return m_amp.value(); }
     // Wave frequency
-    double frequency() const { return m_frequency; }
+    double frequency() const { return m_frequency.value(); }
     // Phase offset
-    double phase() const { return m_phase; }
+    double phase() const { return m_phase.value(); }
     // Amplitude offset
-    double y_offset() const { return m_y_offset; }
+    double y_offset() const { return m_y_offset.value(); }
 };
 
 
@@ -49,17 +62,17 @@ public:
     Builder(const WaveArgs& args) : args(args) {}
 
     // CV qualifier based overload resolution.
-    operator const WaveArgs&() const & { return args; } operator WaveArgs&&() && { return std::move(args); } 
-    Builder& step(double step) { this->args.m_step = step; return *this; }
-    Builder& amp(double amp) { this->args.m_amp = amp; return *this; }
-    Builder& frequency(double frequency) { this->args.m_frequency = frequency; return *this; }
-    Builder& phase(double phase) { this->args.m_phase = phase; return *this; }
-    Builder& y_offset(double y_offset) { this->args.m_y_offset = y_offset; return *this; }
+    operator const WaveArgs&() const & { return args; } operator WaveArgs&&() && { return std::move(args); }
+    Builder& amp(Arg amp) { this->args.m_amp = amp; return *this; }
+    Builder& frequency(Arg frequency) { this->args.m_frequency = frequency; return *this; }
+    Builder& phase(Arg phase) { this->args.m_phase = phase; return *this; }
+    Builder& y_offset(Arg y_offset) { this->args.m_y_offset = y_offset; return *this; }
 };
 
 
 class TimePointGen: public Generator<std::chrono::steady_clock::time_point>
-{ public:
+{
+public:
     std::chrono::steady_clock::time_point get() const override
     {
         return std::chrono::steady_clock::now();
@@ -70,20 +83,33 @@ class SineWave: public Generator<double>
 {
     WaveArgs m_args;
     const Generator<std::chrono::steady_clock::time_point>& m_time_gen;
+    mutable double m_curr_freq;
+    mutable double m_curr_phase;
+    mutable std::chrono::milliseconds m_prev_tp;
 
 public:
     SineWave() = default;
     SineWave(const WaveArgs& args, const Generator<std::chrono::steady_clock::time_point>& gen)
-        : m_args(args), m_time_gen(gen)
-    {}
+        : m_args(args), m_time_gen(gen), m_curr_freq(args.frequency())
+    {
+        m_prev_tp = std::chrono::duration_cast<std::chrono::milliseconds>(gen.get().time_since_epoch());
+    }
 
     double get() const override
     {
         auto tp = m_time_gen.get();
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count();
-        double rad = (ms / 1000. * m_args.frequency()) * 2 * std::numbers::pi_v<double>;
+        auto ms = m_prev_tp.count();
+        m_prev_tp = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch());
+        double rad = (ms / 1000. * m_curr_freq) * 2 * std::numbers::pi_v<double>;
+        double res = std::sin(rad + m_curr_phase) * m_args.amp() + m_args.y_offset();
+        m_curr_freq = m_args.frequency();
+        double rad2 = (ms / 1000. * m_curr_freq) * 2 * std::numbers::pi_v<double>;
+        m_curr_phase = rad2 - rad;
         assert(rad >= 0.);
-        return std::sin(rad + m_args.phase()) * m_args.amp() + m_args.y_offset();
+        assert(rad2 >= 0.);
+        //assert(res + 0.00001 > std::sin(rad2 + m_curr_phase) * m_args.amp() + m_args.y_offset());
+        //assert(res - 0.00001 < std::sin(rad2 + m_curr_phase) * m_args.amp() + m_args.y_offset());
+        return res;
     }
 };
 
@@ -92,21 +118,6 @@ inline constexpr signed char sgn(const T& value)
 {
     return (T{} < value) - (value < T{});
 }
-
-//class SquareWave : public Generator<double>
-//{
-//    const SineWave& m_sine_gen;
-//
-//public:
-//    SquareWave(const SineWave& gen)
-//        : m_sine_gen(gen)
-//    {}
-//
-//    double get() const override
-//    {
-//        return sgn(m_sine_gen.get());
-//    }
-//};
 
 
 class SawToothWave: public Generator<double>
@@ -130,13 +141,13 @@ public:
 };
 
 
-class RingModulator : public Generator<double>
+class Modulator: public Generator<double>
 {
     const Generator<double>& m_carrier;
     const Generator<double>& m_modulator;
 
 public:
-    RingModulator(const Generator<double>& carrier, const Generator<double>& modulator)
+    Modulator(const Generator<double>& carrier, const Generator<double>& modulator)
         : m_carrier(carrier), m_modulator(modulator)
     {}
 
